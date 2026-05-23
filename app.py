ea108b7b3201b1d182879c2fb2a4359e2cb2f427
cejaChris <cejachristopher@gmail.com>
2026-05-23 15:53:38 -0400

    weather fixed

--- app.py
+++ app.py
@@ -219,18 +219,18 @@ if st.session_state['race']:
 
         # WEATHER BREAKS TELEMETRY AND FAST LAP ANALYSIS
 
-        # # WEATHER
+        # WEATHER
 
-        # if 'weather_fig' not in st.session_state:
-        #     st.session_state['weather_fig'] = None
-        # if st.session_state['weather_fig'] == None:
-        #     st.session_state['weather_fig'] = race.plot_weather()
+        if 'weather_fig' not in st.session_state:
+            st.session_state['weather_fig'] = None
+        if st.session_state['weather_fig'] == None:
+            st.session_state['weather_fig'] = race.plot_weather()
         
-        # st.header('Weather', text_alignment='center')
+        st.header('Weather', text_alignment='center')
         
-        # df_format(race.weather_data[1], st)
+        df_format(race.weather_data[1], st)
 
-        # st.plotly_chart(st.session_state['weather_fig'], width='stretch', height='content')
+        st.plotly_chart(st.session_state['weather_fig'], width='stretch', height='content')
 
         restart = st.button('Restart')
         
